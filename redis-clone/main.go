7a8326a9918d839429c23e1527fe5305702b1167
Barak Atias <54084624+brookatlas@users.noreply.github.com>
2023-10-20 21:34:48 +0300

    basic connection and "PING" works

--- redis-clone/main.go
+++ redis-clone/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 )
 
 // use this for help: https://github.com/ngilles/codecrafters-redis-py/blob/master/app/main.py
@@ -53,8 +54,10 @@ func main() {
 }
 
 func handleRequest(connection net.Conn) {
-	buffer := make([]byte, 1024)
+
 	for {
+		buffer := make([]byte, 1024)
+
 		_, err := connection.Read(buffer)
 		if err != nil {
 			error_message_format := "error while reading bytes from connection: %s"
@@ -62,52 +65,133 @@ func handleRequest(connection net.Conn) {
 			log.Fatal(error_message)
 			panic(error_message)
 		}
-	}
 
-	// connection.Write(BYTE ARRAY HERE)
-	// connection.Close()
+		command_array, _ := readValue(buffer)
+		initial_command := command_array[0]
+		var response_pointer *[]byte
+
+		switch initial_command {
+		case "PING":
+			response := ping()
+			response_pointer = &response
+		case "COMMAND":
+			response := command(command_array[1:])
+			response_pointer = &response
+		}
+
+		if response_pointer != nil {
+			connection.Write(*response_pointer)
+		}
+	}
 }
 
-func readRedisRequest(buffer []byte) []byte {
+func readValue(buffer []byte) ([]string, int) {
+	var value_pointer *string
+	var length_pointer *int
+	var array_pointer *[]string
+
 	first_byte := buffer[0]
 	buffer_to_send := buffer[1:]
-	if first_byte == '+' {
-		return readSimpleString(buffer_to_send)
+
+	switch first_byte {
+	case '+':
+		value, length := readSimpleString(buffer_to_send)
+		value_pointer = &value
+		length_pointer = &length
+	case '-':
+		value, length := readError(buffer_to_send)
+		value_pointer = &value
+		length_pointer = &length
+	case ':':
+		value, length := readInteger(buffer_to_send)
+		value_pointer = &value
+		length_pointer = &length
+	case '$':
+		value, length := readBulkString(buffer_to_send)
+		value_pointer = &value
+		length_pointer = &length
+	case '*':
+		value, length := readArray(buffer_to_send)
+		array_pointer = &value
+		length_pointer = &length
+	default:
+		error_message := "message started with an invalid format."
+		log.Fatal(error_message)
+		panic(1)
 	}
-	if first_byte == '-' {
-		return readError(buffer_to_send)
+
+	if value_pointer != nil {
+		string_array := make([]string, 1)
+		string_array[0] = *value_pointer
+		array_pointer = &string_array
 	}
-	if first_byte == ':' {
-		return readInteger(buffer_to_send)
-	}
-	if first_byte == '$' {
-		return readBulkString(buffer_to_send)
-	}
-	if first_byte == '*' {
-		return readArray(buffer_to_send)
-	}
+
+	return *array_pointer, *length_pointer
 }
 
-func readSimpleString(buffer []byte) []byte {
-	stop_index := bytes.Index(buffer, []byte{"\r", "\n"})
+func readSimpleString(buffer []byte) (string, int) {
+	stop_index := bytes.Index(buffer, []byte("\r\n"))
 	value := buffer[0:stop_index]
-	length := len(buffer[0:stop_index]) + 2
 
-	return value, length
+	return string(value), stop_index + 2
 }
 
-func readError(buffer []byte) []byte {
+func readError(buffer []byte) (string, int) {
+	stop_index := bytes.Index(buffer, []byte("\r\n"))
+	value := buffer[0:stop_index]
 
+	return string(value), stop_index + 2
 }
 
-func readInteger(buffer []byte) []byte {
+func readInteger(buffer []byte) (string, int) {
+	stop_index := bytes.Index(buffer, []byte("\r\n"))
+	parsed_value := string(buffer[0:stop_index])
 
+	return parsed_value, stop_index + 2
 }
 
-func readBulkString(buffer []byte) []byte {
+func readBulkString(buffer []byte) (string, int) {
+	string_length, string_start_index := readInteger(buffer)
 
+	parsed_string_length, _ := strconv.Atoi(string_length)
+
+	string_raw := buffer[string_start_index : string_start_index+int(parsed_string_length)]
+
+	return string(string_raw), string_start_index + int(parsed_string_length) + 3
 }
 
-func readArray(buffer []byte) []byte {
+func readArray(buffer []byte) ([]string, int) {
+	buffer_offset := 0
+	token_array := make([]string, 0)
+	array_length, next_buffer_offset := readInteger(buffer[buffer_offset:])
+	buffer_offset = next_buffer_offset
 
+	array_length_parsed, _ := strconv.Atoi(array_length)
+
+	for i := 0; i < array_length_parsed; i++ {
+		current_value, next_buffer_offset := readValue(buffer[buffer_offset:])
+		buffer_offset = buffer_offset + next_buffer_offset
+		token_array = append(token_array, current_value[0])
+	}
+
+	return token_array, len(token_array)
+}
+
+func command(command_array []string) []byte {
+	initial_command := command_array[0]
+
+	switch initial_command {
+	case "DOCS":
+		return command_docs(command_array[1:])
+	default:
+		return []byte("*2\r\n$3\r\nSET\r\n$3\r\nGET")
+	}
+}
+
+func command_docs(command_array []string) []byte {
+	return []byte("*2\r\n$3\r\nSET\r\n%1\r\n$7\r\nsummary\r\n$4\r\nthis\r\n")
+}
+
+func ping() []byte {
+	return []byte("+PONG\r\n")
 }
